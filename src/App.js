61fb87b11ccd372344e9a849eaae1d01c5de5cbd
Rounak Kalbhawar <60340275+rounak-kalbhawar@users.noreply.github.com>
2021-11-25 00:18:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import Users from "./components/cards";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Set initial state
+    this.state = { users_data: [], loading: false };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      2000
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navitems"><div class="btn"><button className="fetchbtn" onClick={this.updateState}>
+              Get Users
+            </button></div>
+          </div>
+        </nav>
+     
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+        <footer className="footer">
+        Let's Grow More Task- 2 
+        </footer>
+        <footer className="footer">
+            By -- Jaya Sharma
+        </footer>
+      </>
+    );
+  }
+}
+
+export default App;
